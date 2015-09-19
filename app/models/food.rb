18bb876d6939813c7a1898e4b7c37834e0897bef
Peter Petrov <petrov.p.peter@gmail.com>
2015-09-19 16:15:50 -0400

    pg search works on food description

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,4 +3,7 @@ class Food < ActiveRecord::Base
   has_many :nutritions
   has_many :nutrients, :through => :nutritions
   has_many :weights
+
+  include PgSearch
+  pg_search_scope :search_by_desc, :against => [:short_desc, :long_desc]
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,12 @@
 class Recipe < ActiveRecord::Base
+
+  
   belongs_to :user
   has_and_belongs_to_many :tags
   has_many :recipe_nutrion_joins
   has_many :nutrients, through: :recipe_nutrient_joins
   has_many :recipe_food_joins
   has_many :foods, through: :recipe_food_joins
+
+
 end
